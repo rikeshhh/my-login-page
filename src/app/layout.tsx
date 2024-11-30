e704e43e522bdbafe2c23580182ab58ea3fd6049
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-12-01 02:40:50 +0545

    fix:unused imports removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Unbounded,Roboto, Josefin_Sans } from 'next/font/google';
+import { Roboto, Josefin_Sans } from 'next/font/google';
 
 import "./globals.css";
 
