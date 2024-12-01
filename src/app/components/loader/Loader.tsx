91dedea671b5225583b3b3bad6e927dc0eba5420
Rikesh Shrestha <114804728+RikeshShresth@users.noreply.github.com>
2024-12-01 20:15:11 +0545

    feat:work on adding on screenshots

--- src/app/components/loader/Loader.tsx
+++ src/app/components/loader/Loader.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Loader = () => {
   return (
-    <section className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 z-50 gap-8">
+    <section className="fixed inset-0 flex flex-col items-center justify-center bg-black  z-50 gap-8">
       <Image
         src="/tribali/TriBali_Logo_Web.png"
         width={100}
